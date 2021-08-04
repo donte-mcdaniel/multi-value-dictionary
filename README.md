a0cf18aef9674945e4e0f5ff41f7569a44143bb7
Donte McDaniel <donte.s.mcdaniel@lmco.com>
2021-08-04 15:07:54 -0400

    Initial commit. Add multi value dictionary

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# multi-value-dictionary
+# Multi Value Dictionary (CLI)
+
+An iteractive dictionary CLI that allows multiple values per key
+
+## Requirements
+
+* [Node.js](https://nodejs.org/) 12 or higher
+* [Git](https://git-scm.com/)
+* [GitHub account](https://github.com/)
+
+## Installation Steps
+
+1. Clone repo
+2. Run `npm run build` to build the project
+3. Run `npm install -g dictionary .` to install the cli globally
+
+## Run Instructions
+
+You can run this CLI by running the following:
+```bash
+dictionary
+```
+
+Valid `Prompt Options` include:
+
+- `KEYS`: string, Gets all the keys from the dictionary
+- `MEMBERS`: string, Gets all members in key
+- `ADD`: string, Adds value to key
+- `REMOVE`: Removes value from key
+- `REMOVEALL`: string, Removes all values in key
+- `CLEAR`: string, Removes all keys and all members from the dictionary
+- `KEYEXISTS`: string, Checks whether a key exists or not
+- `MEMBEREXISTS`: string, Checks whether a member exists within a key
+- `ALLMEMBERS`: string, Returns all the members in the dictionary
+- `ITEMS`: string, Returns all keys in the dictionary and all of their members
+
+
+## License
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/index.ts
@@ -0,0 +1,416 @@
+#!/usr/bin/env node
+'use strict';
+
+// Importing modules
+const inquirer = require('inquirer');
+const colors = require('colors/safe');
+
+// Setting global variables
+let dictionary = new Map;
+
+/**
+ * Main Prompt
+ */
+function mainPrompt() {
+    inquirer.prompt([
+    // questions
+    {
+        type: 'list',
+        message: "Choose an option:",
+        name: "options",
+        choices: ['KEYS', 'MEMBERS', 'ADD', 'REMOVE', 'REMOVEALL', 'CLEAR', 'KEYEXISTS', 'MEMBEREXISTS', 'ALLMEMBERS', 'ITEMS']
+    }
+    ]).then((answers) => {
+        // user feedback
+        console.log(answers.options);
+        switch(answers.options) {
+            case 'KEYS':
+                console.log(keys());
+                mainPrompt();
+                break;
+            case 'ADD':
+                promptAdd();
+                break;
+            case 'REMOVE':
+                promptRemove();
+                break;
+            case 'MEMBERS':
+                promptMembers();
+                break;
+            case 'REMOVEALL':
+                promptRemoveAll();
+                break;
+            case 'CLEAR':
+                console.log(clear());
+                mainPrompt();
+                break;
+            case 'KEYEXISTS':
+                promptKeyExists();
+                break;
+            case 'MEMBEREXISTS':
+                promptMemberExists();
+                break;
+            case 'ALLMEMBERS':
+                console.log(allMembers());
+                mainPrompt();
+                break;
+            case 'ITEMS':
+                console.log(items());
+                mainPrompt();
+                break;
+        }
+    }).catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+}
+
+/**
+ * Prompt for view members of a specific key
+ */
+function promptMembers() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter key:",
+            name: "key"
+        }
+    ]).then((answer) => {
+        console.log(members(answer.key));
+        mainPrompt();
+    }).catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+}
+
+/**
+ * Prompt for adding a key/value
+ */
+function promptAdd() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter key:",
+            name: "key"
+        },
+        {
+            type: 'input',
+            message: "Enter value:",
+            name: "value"
+        }
+    ]).then((answers) => {
+        // console.log(answers.key);
+        console.log(add(answers.key, answers.value));
+        mainPrompt();
+    }).catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+}
+
+/**
+ * Prompt for removing a key/value
+ */
+function promptRemove() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter key:",
+            name: "key"
+        },
+        {
+            type: 'input',
+            message: "Enter value:",
+            name: "value"
+        }
+    ]).then((answers) => {
+        console.log(remove(answers.key, answers.value));
+        mainPrompt();
+    }).catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+}
+
+/**
+ * Prompt for removing a key/value
+ */
+ function promptRemoveAll() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter key:",
+            name: "key"
+        }
+    ]).then((answers) => {
+        console.log(removeAll(answers.key));
+        mainPrompt();
+    }).catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+}
+
+/**
+ * Prompt for checking if key exists
+ */
+ function promptKeyExists() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter key:",
+            name: "key"
+        }
+    ]).then((answers) => {
+        console.log(keyExists(answers.key));
+        mainPrompt();
+    }).catch((error) => {
+        if (error.isTtyError) {
+            console.log(error);
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+            console.log(error);
+        }
+    });
+}
+
+/**
+ * Prompt for checking if member exists
+ */
+ function promptMemberExists() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter key:",
+            name: "key"
+        },
+        {
+            type: 'input',
+            message: "Enter value:",
+            name: "value"
+        }
+    ]).then((answers) => {
+        console.log(memberExists(answers.key, answers.value));
+        mainPrompt();
+    }).catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+}
+
+/**
+ * Adds value to key
+ * @param key dictionary key
+ * @param value value being added
+ * @returns {String} String
+ */
+function add(key: String, value: String) {
+    if (dictionary.has(key)) {
+        let valuesArray = Array.from(dictionary.get(key));
+        if (valuesArray.indexOf(value) !== -1) {
+            return colors.red('ERROR, member already exists for key');
+        }
+        else {
+            valuesArray.push(value);
+            dictionary.set(key, valuesArray);
+        }
+    }
+    else {
+        dictionary.set(key, [value]);
+    }
+    
+    return colors.green('Added');
+}
+
+/**
+ * Removes value from key
+ * @param key dictionary key
+ * @param value value being removed
+ * @returns {String} String
+ */
+function remove(key: String, value: String) {
+    if (dictionary.has(key)) {
+        let valuesArray: Array<String> = Array.from(dictionary.get(key));
+
+        if (valuesArray.length === 1) {
+            dictionary.delete(key);
+        }
+        if (valuesArray.length > 1) {
+            const index = valuesArray.indexOf(value);
+            if (index > -1) {
+                valuesArray.splice(index, 1);
+            }
+            else {
+                return colors.red('ERROR, member does not exist');
+            }
+            dictionary.set(key, valuesArray);
+        }
+    }
+    else {
+        return colors.red('ERROR, key does not exist');
+    }
+    return colors.green('Removed');
+}
+
+/**
+ * Gets all members in key
+ * @param key dictionary key
+ * @returns {String} String
+ */
+function members(key: String) {
+    if (dictionary.has(key)) {
+        const members = Array.from(dictionary.get(key));
+        let array = [];
+
+        for (let i = 0; i < members.length; i++) {
+            array.push(`${i+1}) ${members[i]} \n`);
+        }
+
+        return colors.green(array.join('').toString());
+    }
+    else {
+        return colors.red('ERROR, key does not exist.');
+    }
+}
+
+/**
+ * Gets all the keys from the dictionary
+ * @returns {String} String
+ */
+function keys() {
+    const keys = Array.from(dictionary.keys());
+
+    if (keys.length > 0) {
+        let array = [];
+        for (let i = 0; i < keys.length; i++) {
+            array.push(`${i+1}) ${keys[i]} \n`);
+        }
+    
+        return colors.green(array.join('').toString());
+    }
+    else {
+        return colors.red('(empty set)');
+    }
+}
+
+/**
+ * Removes all values in key
+ * @param key dictionary key
+ * @returns {String} String
+ */
+function removeAll(key: String) {
+    if (dictionary.has(key)) {
+        dictionary.delete(key);
+        return colors.green(`Removed All ${key}`);
+    }
+    else {
+        return colors.red('ERROR, key does not exist');
+    }
+}
+
+/**
+ * Removes all keys and all members from the dictionary
+ * @returns {String} String
+ */
+function clear() {
+    dictionary.clear();
+    return colors.green('Cleared');
+}
+
+/**
+ * Checks whether a key exists or not
+ * @param key dictionary key
+ * @returns {String} String
+ */
+function keyExists(key: String) {
+    if (dictionary.has(key)) {
+        return colors.green('true');
+    }
+    else {
+        return colors.red('false');
+    }
+}
+
+/**
+ * Checks whether a member exists within a key
+ * @param key dictionary key
+ * @param value member to check
+ * @returns {String} String
+ */
+function memberExists(key: String, value: String) {
+    const members = Array.from(dictionary.get(key));
+    const index = members.indexOf(value);
+    if (index > -1) {
+        return colors.green('true');
+    }
+    else {
+        return colors.red('false');
+    }
+}
+
+/**
+ * Returns all the members in the dictionary
+ * @returns {String} String
+ */
+function allMembers() {
+    const keys = Array.from(dictionary.keys());
+    let array = [];
+    let count = 1;
+
+    keys.forEach(key => {
+        const members = Array.from(dictionary.get(key));
+
+        for (let i = 0; i < members.length; i++) {
+            array.push(`${count}) ${members[i]} \n`);
+        }
+
+        count++;
+    });
+
+    return colors.green(array.join('').toString());
+}
+
+/**
+ * Returns all keys in the dictionary and all of their members
+ * @returns {String} String
+ */
+function items() {
+    const keys = Array.from(dictionary.keys());
+    let array = [];
+    let count = 1;
+
+    keys.forEach(key => {
+        const members = Array.from(dictionary.get(key));
+
+        for (let i = 0; i < members.length; i++) {
+            array.push(`${count}) ${key}: ${members[i]} \n`);
+        }
+
+        count++;
+    });
+
+    return colors.green(array.join('').toString());
+}
+
+// start
+mainPrompt();
